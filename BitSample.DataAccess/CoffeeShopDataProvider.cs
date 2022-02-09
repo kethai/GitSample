325c089e442bbb9662608897b9e41ae71ae3dd04
Ke Thai <kthai@crea.ca>
2022-02-09 14:24:31 -0500

    Add access data project to solution

---
+++ BitSample.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using BitSample.DataAccess.Model;
+using System.Collections.Generic;
+
+namespace BitSample.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop{Location = "Frankfurk", BeansInStockInKg = 107};
+            yield return new CoffeeShop{Location = "Freiburg", BeansInStockInKg = 23};
+            yield return new CoffeeShop{Location = "Munich", BeansInStockInKg = 56};
+        }
+    }
+}
